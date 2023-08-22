38cc412ec4442f6ef6c749d6538647cd23560f5c
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2023-08-22 01:23:43 -0300

    :wrench: Adjust login screen

--- Program.java
+++ Program.java
@@ -10,15 +10,16 @@ public class Program {
         DataManager<User> userManager = new DataManager<>(User.class);
         userManager.create(new UserAdmin("teste1", "teste", "TESTE", "TESTADOR"));
 
-        UserAuthenticator auth = new UserAuthenticator();
+        //UserAuthenticator auth = new UserAuthenticator();
 
-        User user1 = auth.authenticate("teste1", "teste");
-        User user2 = auth.authenticate("teste", "testando");
+        //User user1 = auth.authenticate("teste1", "teste");
+        //User user2 = auth.authenticate("teste", "testando");
 
-        System.out.println(user1);
-        System.out.println(user2);
+        //System.out.println(user1);
+        //System.out.println(user2);
 
         TelaLogin telaLogin = new TelaLogin();
+        telaLogin.setLocationRelativeTo(null);
         telaLogin.setVisible(true);
 
     }
--- view/TelaLogin.java
+++ view/TelaLogin.java
@@ -9,56 +9,67 @@ import model.User;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
-import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
-import java.awt.SystemColor;
 import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class TelaLogin extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
-	
 	private JPanel contentPane;
 	private JTextField tfUser;
 	private JPasswordField pwfUser;
 
 	public TelaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(400, 250, 520, 300);
+		setBounds(400, 250, 450, 319);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 414, 259);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
 		JLabel lblNewLabel = new JLabel("Fazer login");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 18));
-		lblNewLabel.setBounds(209, 11, 105, 21);
-		contentPane.add(lblNewLabel);
+		lblNewLabel.setBounds(157, 17, 97, 25);
+		panel.add(lblNewLabel);
+		
+		JLabel lblUsurio = new JLabel("Usuário");
+		lblUsurio.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		lblUsurio.setBounds(128, 59, 74, 14);
+		panel.add(lblUsurio);
 		
 		tfUser = new JTextField();
 		tfUser.setBackground(SystemColor.inactiveCaption);
-		tfUser.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		tfUser.setBounds(173, 67, 181, 28);
-		contentPane.add(tfUser);
+		tfUser.setBounds(128, 84, 168, 29);
+		panel.add(tfUser);
 		tfUser.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Senha");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(128, 130, 46, 14);
+		panel.add(lblNewLabel_1);
 		
 		pwfUser = new JPasswordField();
 		pwfUser.setEchoChar('*');
 		pwfUser.setBackground(SystemColor.inactiveCaption);
-		pwfUser.setToolTipText("");
-		pwfUser.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		pwfUser.setBounds(173, 123, 181, 28);
-		contentPane.add(pwfUser);
+		pwfUser.setBounds(128, 155, 168, 32);
+		panel.add(pwfUser);
 		
 		JButton btnLogin = new JButton("Entrar na conta");
 		btnLogin.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		        String username = tfUser.getText();
+			public void actionPerformed(ActionEvent e) {
+				String username = tfUser.getText();
 		        char[] passwordChars = pwfUser.getPassword();
 		        String password = new String(passwordChars);
 
@@ -69,22 +80,13 @@ public class TelaLogin extends JFrame {
 		        if (authenticatedUser != null) {
 		            JOptionPane.showMessageDialog(null, "Login bem-sucedido!");
 		        } else {
-		            JOptionPane.showMessageDialog(null, "Falha no login. Verifique suas credenciais.");
+		            JOptionPane.showMessageDialog(null, "Falha no login. Verifique suas credenciais.", "Aviso", JOptionPane.WARNING_MESSAGE);
 		        }
-		    }
+			}
 		});
+		btnLogin.setBackground(new Color(255, 255, 255));
 		btnLogin.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		btnLogin.setBounds(178, 202, 158, 28);
-		contentPane.add(btnLogin);
-		
-		JLabel lblNewLabel_1 = new JLabel("Usuário");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		lblNewLabel_1.setBounds(86, 74, 57, 14);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Senha");
-		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		lblNewLabel_1_1.setBounds(86, 130, 57, 14);
-		contentPane.add(lblNewLabel_1_1);
+		btnLogin.setBounds(128, 217, 155, 31);
+		panel.add(btnLogin);
 	}
 }
